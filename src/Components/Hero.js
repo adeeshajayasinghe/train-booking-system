09830887483625c23e517f364bc117106a62c763
Adeesha Jayasinghe <jayasingheai.20@uom.lk>
2023-08-03 20:09:26 +0530

    Added the main search box.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,24 +1,33 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context'
+import Searchbox from './Searchbox';
 const Hero = () => {
   const {closeSubMenu} = useContext(AppContext);
   return (
     <section className='hero' onMouseOver={closeSubMenu}>
       <div className='hero-center'>
         <article className='hero-info'>
-          <h1>
+          <br />
+          <br />
+          <br />
+          <br />
+          <h1 className='welcome-text'>
             Welocome to <br />
             Sri Lanka Railways
           </h1>
-          <p>
+          <p className='sub-text'>
             Adventure awaits! Hop aboard and let the journey begin.
           </p>
-          <button className='btn'>Book Now</button>
+          {/* <button className='btn'>Book Now</button> */}
+          <a href='#searchbox'>
+            <button className='btn'>Book Now</button>
+          </a>
         </article>
         {/* <article className='hero-images'>
           <img src={phoneImg} className='phone-img' alt='phone' />
         </article> */}
       </div>
+      <Searchbox/>
     </section>
   );
 }
