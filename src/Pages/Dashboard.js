a01c891fd1b375161366cfc817772a4e3fc68ff6
RashminiJayasundara <141397687+RashminiJayasundara@users.noreply.github.com>
2023-09-03 21:17:46 +0530

    Update Dashboard.js
    how to go to form

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {AppContext} from '../context'
 import { useContext } from 'react';
 import Train from '../images/Train.png';
+import {Link} from 'react-router-dom';
 const Dashboard = () => {
   const {trainList} = useContext(AppContext);
   return (
@@ -12,7 +13,9 @@ const Dashboard = () => {
         <div key={train._id} className="train-card">
           <h2>{train.trainName}</h2>
           <div className='train-img'>
-            <img src={Train} alt={train.trainName} />
+            <Link to={'/Form'}>
+           <img src={Train} alt={train.trainName} />
+           </Link>
           </div>
           <p>Train No: {train.trainNo}</p>
           <p>Origin: {train.origin}</p>
