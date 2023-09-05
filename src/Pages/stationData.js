7840bbcb6852be556dde882848a08bc1ed585182
RashminiJayasundara <141397687+RashminiJayasundara@users.noreply.github.com>
2023-09-05 23:13:11 +0530

    stations data list

---
+++ src/Pages/stationData.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+    const stations = [
+        { label: 'A' },
+        { label: 'B' },
+        { label: 'C' },
+        { label: 'D' },
+        { label: 'E' },
+        { label: 'F' },
+        { label: 'G' },
+        { label: 'H' },
+        { label: 'I' },
+        { label: 'J' },
+        { label: 'K' },
+        { label: 'L' },
+        { label: 'M' },
+        { label: 'N' },
+        { label: 'O' },
+        { label: 'P' },
+        { label: 'Q' },
+        { label: 'R' },
+        { label: 'S' },
+        { label: 'T' },
+        { label: 'U' },
+        { label: 'V' },
+        { label: 'W' },
+        { label: 'X' },
+        { label: 'Y' },
+        { label: 'Z' }
+    ];
+export default stations;
