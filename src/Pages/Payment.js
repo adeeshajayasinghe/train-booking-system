60a2bb5a890e4e954756021430fa33b83497107b
Adeesha Jayasinghe <jayasingheai.20@uom.lk>
2023-09-09 16:22:16 +0530

    Payment page created.

--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -1,8 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Paymentimg from '../images/Payments.png';
+import visacard from '../images/visa.png';
+import mastercard from '../images/master.png';
+import Button from '@mui/material/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
 
 const Payment = () => {
+  const [selectedCard, setSelectedCard] = useState(null);
+  const handleCardSelect = (cardType) => {
+    setSelectedCard(cardType);
+  };
+  const cardStyles = (cardType) => {
+    return {
+      border: selectedCard === cardType ? '2px solid #007bff' : '2px solid transparent',
+    };
+  };
   return (
-    <div>Payment</div>
+    <section className='hero'>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>Payment Gateway</h2>
+          <p>Make your payment here with secure.</p>
+          <article className='hero-images'>
+            <img src={Paymentimg} className='login-img' alt='Payment-image' />
+          </article>
+        </article>
+        <form className='review-reg'>
+          <p>Select your card type</p>
+          <div className="payment-card-options">
+            <div>
+              <img src={visacard} alt="Visa"  className="payment-card"
+              style={cardStyles('visa')}
+              onClick={() => handleCardSelect('visa')}/>
+            </div>
+            <div>
+              <img src={mastercard} alt="Mastercard" className="payment-card"
+              style={cardStyles('mastercard')}
+              onClick={() => handleCardSelect('mastercard')}/>
+            </div>
+          </div>
+          <FormControl sx={{ width: 460, ml:2.9}}>
+            <FormLabel>Card Number</FormLabel>
+            <Input placeholder="0000-0000-0000-0000" variant="soft"/>
+          </FormControl>
+          <div className='route'>
+                <div className='origin'>
+                    <FormControl>
+                        <FormLabel>Expiry</FormLabel>
+                        <Input placeholder="MM/YY" variant="soft"/>
+                    </FormControl>
+                </div>
+                <div className='dest'>
+                    <FormControl>
+                        <FormLabel>CVC</FormLabel>
+                        <Input placeholder="CVC" variant="soft"/>
+                    </FormControl>
+                </div>
+            </div>
+          <Button sx={{
+                    width: 170,
+                    height: 40,
+                    borderRadius: '5px',
+                    margin: '0 10px 10px 0', 
+                  }}
+                  variant='contained'
+                  >
+                Pay Now
+          </Button>
+          <Button sx={{
+                    width: 170,
+                    height: 40,
+                    borderRadius: '5px',
+                    margin: '0 10px 10px 0', 
+                  }}
+                  variant='outlined' 
+                  >
+                Cancel
+              </Button>
+      </form>
+      </div>
+    </section>
   )
 }
 
