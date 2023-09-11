1c6655e6c399b50a2df7381992de2d709a9c108b
KMjanith <janithjmk.20@uom.lk>
2023-09-11 23:16:41 +0530

    booking detaiails back end api modified

--- Back-end/src/Models/Booking.js
+++ Back-end/src/Models/Booking.js
@@ -3,6 +3,10 @@ const Joi = require('joi');
 
 
 const BookingSchema = new mongoose.Schema({
+    ReferenceNo: {
+        type: String,
+        require: true,
+    },
     firstName: {
         type: String,
         required: true,
@@ -50,6 +54,26 @@ const BookingSchema = new mongoose.Schema({
     date:{
         type: String,
         require:true
+    },
+    price:{
+        type: String,
+        require:true
+    },
+    seat_numbers: {
+        type: Array,
+        require:true
+    },
+    class: {
+        type: String,
+        require:true
+    },
+    timeFrom: {
+        type: String,
+        require:true
+    },
+    timeTo: {
+        type: String,
+        require:true
     }
 
 });
@@ -58,16 +82,24 @@ const Booking = mongoose.model('BookingHistory', BookingSchema);
 
 function validateBooking(booking){
     const schema = Joi.object({
+        ReferenceNo: Joi.string().required(),
         firstName: Joi.string().min(5).max(255).required(),
         lastName: Joi.string().min(5).max(255).required(),
         mobile: Joi.string().min(10).max(10).required(),
         email: Joi.string().required().email(),
         NIC:Joi.string().required().min(10).max(10),
         passengerCount:Joi.required(),
-        trainName:Joi.string().required(),
+        trainName: Joi.string().required(),
+        trainNo:Joi.string().required(),
         from:Joi.string().required(),
         to:Joi.string().required(),
-        date:Joi.string().required()
+        date: Joi.string().required(),
+        price: Joi.string().required(),
+        seat_numbers: Joi.array().required(),
+        class: Joi.string().required(),
+        timeFrom: Joi.string().required(),
+        timeTo: Joi.string().required()
+        
 
     });
     return schema.validate(booking);
--- Back-end/src/Routes/booking.js
+++ Back-end/src/Routes/booking.js
@@ -5,12 +5,14 @@ const _ = require('lodash');
 
 
 router.post('/', async (req, res) => {
-    
+    console.log(req.body);
     const {error} = validate(req.body);
     if (error) {
         return res.status(400).json({ error: error.details[0].message });
     }
-    let booking = new Booking(_.pick(req.body, ['firstName', 'lastName', 'mobile', 'email', 'NIC','passengerCount','trainName','from','to','date']));
+    let booking = new Booking(_.pick(req.body, ['ReferenceNo','firstName', 'lastName', 'mobile', 'email', 'NIC', 'passengerCount', 'trainName', 'trainNo', 'from', 'to', 'date', 'price', 'seat_numbers', 'class', 'timeFrom', 'timeTo']));
+    
+    
     
     await booking.save();
     res.status(200).send("successfully booked!");
