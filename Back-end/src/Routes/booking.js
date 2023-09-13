64e7c35d052a41e57a642a670b8b0f6c7e75df7f
KMjanith <kavinduj.20@cse.mrt.ac.lk>
2023-09-13 07:04:01 +0530

    ref number logic added

--- Back-end/src/Routes/booking.js
+++ Back-end/src/Routes/booking.js
@@ -2,7 +2,24 @@ const express = require('express');
 const router = express.Router();
 const {Booking, validate} = require('../Models/Booking');
 const _ = require('lodash');
+const c = require('config');
 
+//ref no generation
+function generateRef() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hour = String(now.getHours()).padStart(2, '0');
+  const minute = String(now.getMinutes()).padStart(2, '0');
+  const second = String(now.getSeconds()).padStart(2, '0');
+  
+  const formattedDate = `${year}${month}${day}`;
+  const formattedTime = `${hour}${minute}${second}`;
+  const referenceNumber = `TBS/${formattedDate}/${formattedTime}`;
+  
+  return referenceNumber;
+}
 
 router.post('/', async (req, res) => {
     console.log(req.body);
@@ -10,6 +27,12 @@ router.post('/', async (req, res) => {
     if (error) {
         return res.status(400).json({ error: error.details[0].message });
     }
+
+    let Ref_no = generateRef();
+    console.log(req.body);
+    console.log(Ref_no);
+    req.body.ReferenceNo = Ref_no;
+    console.log(req.body);
     let booking = new Booking(_.pick(req.body, ['ReferenceNo','firstName', 'lastName', 'mobile', 'email', 'NIC', 'passengerCount', 'trainName', 'trainNo', 'from', 'to', 'date', 'price', 'seat_numbers', 'class', 'timeFrom', 'timeTo']));
     
     
